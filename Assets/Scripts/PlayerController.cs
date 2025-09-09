6be5f2746ed3b300589fdc157370bdfb2ff4d7ff
Samarth <samarth@Samarths-MacBook-Air.local>
2025-09-09 19:08:04 +0530

    Test

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(0, 0, 0);
+        Debug.Log(move.x);
     }
 }
